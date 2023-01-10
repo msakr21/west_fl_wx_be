859cf36c66852e77f5c0b08e4ffe70498d3b23f0
Joseph Hilby <joseph.hilby@gmail.com>
2023-01-10 09:10:10 -0600

    Move endpoints to api/v1 and update tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :create]
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:index, :show, :create]
       get 'EscambiaCountyFLAlerts', to: 'nws_alerts#index'
     end
   end
